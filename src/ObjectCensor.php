e714c5fa5988f77cc98ed1a39858ccd31458b644
jorgebertran87 <jorgebertran87@gmail.com>
2018-11-02 20:08:36 +0100

    Pass test for ObjectCensor

--- src/ObjectCensor.php
+++ src/ObjectCensor.php
@@ -2,9 +2,36 @@
 
 namespace Censor;
 
+use Censor\Domain\CensoredWord;
+use Censor\Domain\CensoredWords;
+use Censor\Domain\Text;
+use Censor\Domain\Word;
+
 class ObjectCensor implements CensorInterface
 {
     public function __invoke(array $censoredWords, string $text) : string
     {
+        $text          = $this->fromStringToText($text);
+        $censoredWords = $this->fromArrayToCollection($censoredWords);
+
+        $text->applyCensoredWords($censoredWords);
+        return $text->censored();
+    }
+
+    private function fromArrayToCollection(array $censoredWords): CensoredWords
+    {
+        $censoredWords = array_map(
+            function(string $item)
+            {
+                return new CensoredWord(new Word($item));
+            },
+            $censoredWords
+        );
+        return CensoredWords::create($censoredWords);
+    }
+
+    private function fromStringToText(string $text): Text
+    {
+        return new Text($text);
     }
 }
